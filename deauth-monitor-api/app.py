29529dec0fd135623d5c78bd0e61952d49caf3a2
Trentin Gillis <craishot@gmail.com>
2019-10-25 16:21:00 -0600

    Update flask api to return a set of mock data

--- deauth-monitor-api/app.py
+++ deauth-monitor-api/app.py
@@ -2,9 +2,53 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+mock_data = {
+  "lastAttackTime": "Oct 12 12:43 MST",
+  "lastAttackDuration": "13m 27s",
+  "lastAttackAttackerMAC": "00:14:22:01:23:45",
+  "lastAttackClientMAC": "00:99:99:00:99:00",
+  'deauthAttacks': [
+    {
+      'id': 1,
+      'timeOccurred': 'Oct 12 12:43 MST',
+      'attackDuration': '27m 22s',
+      'attackerMAC': '00:14:22:01:23:45',
+      'clientMAC': '00:99:99:00:99:00'
+    },
+    {
+      'id': 1,
+      'timeOccurred': 'Oct 12 12:43 MST',
+      'attackDuration': '27m 22s',
+      'attackerMAC': '00:14:22:01:23:45',
+      'clientMAC': '00:99:99:00:99:00'
+    },
+    {
+      'id': 1,
+      'timeOccurred': 'Oct 12 12:43 MST',
+      'attackDuration': '27m 22s',
+      'attackerMAC': '00:14:22:01:23:45',
+      'clientMAC': '00:99:99:00:99:00'
+    },
+    {
+      'id': 1,
+      'timeOccurred': 'Oct 12 12:43 MST',
+      'attackDuration': '27m 22s',
+      'attackerMAC': '00:14:22:01:23:45',
+      'clientMAC': '00:99:99:00:99:00'
+    },
+    {
+      'id': 1,
+      'timeOccurred': 'Oct 12 12:43 MST',
+      'attackDuration': '27m 22s',
+      'attackerMAC': '00:14:22:01:23:45',
+      'clientMAC': '00:99:99:00:99:00'
+    }
+  ]
+}
+
 @app.route('/')
 def index():
-  return jsonify({'message': 'Hello world!'})
+  return jsonify(mock_data)
 
 if __name__ == '__main__':
   app.run()
