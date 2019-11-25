f4fe572500fdfd7ab383af32c3ebd58ea7fd5785
Cameron Cooper <cameron.w.cooper@gmail.com>
2019-11-24 19:05:48 -0700

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
-airmon-ng start wlan1
+airmon-ng start wlan1 &
 
 # Start the scapy application
 cd deauth-monitor-scapy
 pip3 install -r requirements.txt
-python3 deauth.py
+python3 deauth.py &
 cd ..
 
 # Start the Flask API
 cd deauth-monitor-api
 pip3 install -r requirements.txt
-python3 app.py reset_db
+python3 app.py reset_db &
 cd ..
 
 # Start the React application
 cd deauth-monitor-web/deauth-web-ui
 npm install
-npm run build
+npm run build 
 cd ..
 npm install
-node index.js
+node index.js &
